ace0563ce40eb55a6b1b0e30e8747ddaba66dab2
Paula Kamisato <paukamisato@gmail.com>
2021-10-07 12:21:23 +0200

    deploy

--- server/app.js
+++ server/app.js
@@ -30,4 +30,7 @@ app.use("/api", allRoutes);
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
 
+app.use(express.static(path.join(__dirname, "public")));
+app.use((req, res) => res.sendFile(__dirname + "/public/index.html"));
+
 module.exports = app;
